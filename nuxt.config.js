2ab1d6ec24ad016067cfb9461f9ef9a0fd07ca73
ghost <ghost.vkv@gmail.com>
2022-02-22 17:22:11 +0300

    Fix: https in config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,23 @@
-// import fs from 'fs'
+import fs from 'fs'
 import { directive } from 'v-aspect-ratio/dist/v-aspect-ratio.ssr.esm'
 
-// const server =
-//   process.env.NODE_ENV === 'production'
-//     ? {
-//         https: {
-//           key: fs.readFileSync('/etc/letsencrypt/live/anastasi-target.ru/privkey.pem'),
-//           cert: fs.readFileSync('/etc/letsencrypt/live/anastasi-target.ru/cert.pem'),
-//           ca: fs.readFileSync('/etc/letsencrypt/live/anastasi-target.ru/chain.pem'),
-//         },
-//       }
-//     : {}
-
-const server = {}
+const server =
+  process.env.NODE_ENV === 'production' && !process.env.LOCAL
+    ? {
+        https: {
+          key: fs.readFileSync(
+            '/etc/letsencrypt/live/anastasi-target.ru/privkey.pem'
+          ),
+          cert: fs.readFileSync(
+            '/etc/letsencrypt/live/anastasi-target.ru/cert.pem'
+          ),
+          ca: fs.readFileSync(
+            '/etc/letsencrypt/live/anastasi-target.ru/chain.pem'
+          ),
+        },
+        host: 'anastasi-target.ru'
+      }
+    : {}
 
 server.port = process.env.PORT
 
