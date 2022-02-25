819c2d01a5597b980f4bc465a1dc9e169a6c7fcb
ghost <ghost.vkv@gmail.com>
2022-02-25 16:28:28 +0300

    Add "show_node" query param listener.

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ server.port = process.env.PORT
 export default {
   server: server,
 
-  target: 'static',
+  // target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
--- plugins/vue2-smooth-scroll.js
+++ plugins/vue2-smooth-scroll.js
@@ -1,6 +1,43 @@
 import Vue from 'vue'
 import VueSmoothScroll from 'vue2-smooth-scroll'
 
-Vue.use(VueSmoothScroll, {
-  offset: 70
-})
+Vue.use(VueSmoothScroll, { offset: 70 })
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+const getElement = (nodeId) => document.querySelector(`#${nodeId}`)
+
+export default async function ({ route }) {
+  if (!process.client) {
+    return
+  }
+
+  const nodeId = route.query?.show_node
+
+  if (!nodeId) {
+    return
+  }
+
+  await sleep(1000)
+  let el = getElement(nodeId)
+
+  let i = 0
+  while (!el || i < 3) {
+    el = getElement(nodeId)
+
+    if (!el) {
+      await sleep(1000)
+    }
+
+    i += 1
+  }
+
+  if (!el) {
+    return
+  }
+
+  Vue.prototype.$smoothScroll({
+    scrollTo: el,
+    hash: `#${nodeId}`,
+  })
+}
